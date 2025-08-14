2007b52460d1e5b26d242fe064c94f32c1b332a1
Zliter <110454597+Zliter@users.noreply.github.com>
2025-08-14 22:11:43 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,6 @@
-// 先占位，等会儿换成你自己的值
-const LC_APP_ID  = 'dsUbx0qZY2bm2JIVbkkMz57B-MdYXbMMI';
-const LC_APP_KEY = '1ZcwVDwv4NQsSEbKlwvFE3l6';
-const LC_SERVER  = 'https://dsubx0qz.api.lncldglobal.com';
-
-const DELETE_PIN = '';   // 可选：删图 PIN（留空=任何人可删）
-const PAGE_LIMIT = 500;
-
-AV.init({ appId: LC_APP_ID, appKey: LC_APP_KEY, serverURL: LC_SERVER });
+// LeanCloud 国内版配置
+AV.init({
+  appId: 'NQGOpixquGI0KSXYOiyasWsF-gzGzoHsz',
+  appKey: 'pA03OwFMvaKyGd2HOPTdxq9V',
+  serverURL: 'https://nqgopixq.lc-cn-n1-shared.com' // 例如 https://xxx.lc-cn-n1-shared.com
+});
